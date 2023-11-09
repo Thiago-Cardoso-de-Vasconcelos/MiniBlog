27bcfb483398b494e8ca21ef07e2e3eee7a7e6c0
Thiago Vasconcelos <thiagocardosodevasconcelos@outlook.com>
2023-11-09 03:51:44 -0300

    feat:criando  página de edição

--- src/App.jsx
+++ src/App.jsx
@@ -1,66 +1,81 @@
-import './App.css';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import { onAuthStateChanged } from 'firebase/auth';
+import "./App.css";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { onAuthStateChanged } from "firebase/auth";
 
 //hooks
 import { useState, useEffect } from "react";
 import { useAuthentication } from "./hooks/useAuthentication";
 
 //context
-import { AuthProvider } from './context/AuthContext';
+import { AuthProvider } from "./context/AuthContext";
 
 //pages
-import Home from './pages/Home/Home';
-import About from './pages/About/About';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import Login from './pages/Login/Login';
-import Register from './pages/Register/Register';
-import CreatePost from './pages/CreatePost/CreatePost';
-import Dashboard from './pages/Dashboard/Dashboard';
-import Search from './pages/Search/Search';
-import Post from './pages/Post/Post';
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Login from "./pages/Login/Login";
+import Register from "./pages/Register/Register";
+import CreatePost from "./pages/CreatePost/CreatePost";
+import EditPost from "./pages/EditPost/EditPost";
+import Dashboard from "./pages/Dashboard/Dashboard";
+import Search from "./pages/Search/Search";
+import Post from "./pages/Post/Post";
 
 function App() {
+  const [user, setUser] = useState(undefined);
+  const { auth } = useAuthentication();
 
-  const [user, setUser] = useState(undefined)
-  const {auth} = useAuthentication()
+  const loadingUser = user === undefined;
 
-  const loadingUser = user === undefined
-
-  useEffect(()=> {
+  useEffect(() => {
     onAuthStateChanged(auth, (user) => {
-      setUser(user)
-    })
-  },[auth]);
+      setUser(user);
+    });
+  }, [auth]);
 
-  if(loadingUser) {
+  if (loadingUser) {
     return <p>Carregando...</p>;
   }
 
   return (
-    <div className='app'>
+    <div className="app">
       <AuthProvider value={{ user }}>
-      <BrowserRouter>
-      <Navbar/>
-        <div className='container'>
-          <Routes>
-            <Route path='/' element={<Home/>}/>
-            <Route path='/about' element={<About/>}/>
-            <Route path='/search' element={<Search/>}/>
-            <Route path='/posts/:id' element={<Post/>}/>
-            <Route path='/login' element={!user ? <Login/> : <Navigate to="/"/>}/>
-            <Route path='/register' element={!user ? <Register/> : <Navigate to="/"/>}/>
-            <Route path='/posts/create' element={user ? <CreatePost/> : <Navigate to="/login"/>}/>
-            <Route path='/dashboard' element={user ? <Dashboard/> : <Navigate to="/login"/>}/>
-          </Routes>
-        </div>
-        <Footer/>
-      </BrowserRouter>
+        <BrowserRouter>
+          <Navbar />
+          <div className="container">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/search" element={<Search />} />
+              <Route path="/posts/:id" element={<Post />} />
+              <Route
+                path="/login"
+                element={!user ? <Login /> : <Navigate to="/" />}
+              />
+              <Route
+                path="/register"
+                element={!user ? <Register /> : <Navigate to="/" />}
+              />
+              <Route
+                path="/posts/edit/:id"
+                element={user ? <EditPost/> : <Navigate to="/login" />}
+              />
+              <Route
+                path="/posts/create"
+                element={user ? <CreatePost /> : <Navigate to="/login" />}
+              />
+              <Route
+                path="/dashboard"
+                element={user ? <Dashboard /> : <Navigate to="/login" />}
+              />
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
       </AuthProvider>
-
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
